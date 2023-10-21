965ba0221e0472d004fd937392b2bb5e901308a5
Per-Olof Bengtsson <po@niffic.se>
2023-10-21 09:10:10 +0200

    make use of the globalThis so that it is easy to get the game object in the browser conosole

--- html/quest.mjs
+++ html/quest.mjs
@@ -2,5 +2,5 @@ import { Game } from './game.mjs'
 import { Screen } from './screen.mjs'
 
 const screen = new Screen(document.getElementById('gameView'))
-const game = new Game(window, screen, console.log)
+globalThis.game = new Game(window, screen, console.log)
 game.start()
