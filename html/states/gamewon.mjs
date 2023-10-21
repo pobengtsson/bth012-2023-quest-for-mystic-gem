ceba853861add8cb7bbda36aabb444a40bf016b2
Per-Olof Bengtsson <po@niffic.se>
2023-10-21 08:18:32 +0200

    added Player won state and screen with button to start a new game

--- html/states/gamewon.mjs
+++ html/states/gamewon.mjs
@@ -1,7 +1,15 @@
 import { State } from './state.mjs'
 
 export class GameWon extends State {
-  execute () {
-    console.log('Win')
+  async loadView () {
+    const response = await fetch('success.html')
+    const successHtml = await response.text()
+    this.screen.classList = ['success']
+    this.screen.htmlTemplate = successHtml
+
+    const startButton = document.getElementById('continue')
+    startButton.addEventListener('click', () => {
+      this.game.start();
+    })
   }
 }
